7d37d65d67124d3ed8192094d68d2fd0574835c5
BekaBadzagua <beka.badzagua@gmail.com>
2020-11-25 20:15:59 +0400

    demo

---
+++ Lectures/Lecture 3 JavaScript/Lecture5.js
@@ -0,0 +1,18 @@
+function log(content){
+    console.log(content)
+}
+function space(){
+    console.log()
+    console.log()
+    console.log()
+}
+
+
+/////////// Lecture 5
+
+
+
+// Recording დასსაწყებია
+
+
+
